bbf2d13e9f68501fe122cb7fc2d8c61796190590
Victor-Agbo <victoragbo72gmail.com>
2021-02-26 01:16:17 +0100

    Update

--- planit/settings.py
+++ planit/settings.py
@@ -139,3 +139,8 @@ DATABASES['default'].update(db_from_env)
 WHITENOISE_USE_FINDERS = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import django_heroku
+django_heroku.settings(locals())
+
+STATIC_ROOT='staticfiles'
