9804ea3e2d347a43ef5f07eab9ee816b92403e65
Victor-Agbo <victoragbo72@gmail.com>
2021-03-09 02:34:07 +0100

    Update

--- planit/settings.py
+++ planit/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'planit.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -93,7 +93,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
