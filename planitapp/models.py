18d6f342d09765c9e81ef53ab122a4ad7760e0e1
Victor-Agbo <74287630+Victor-Agbo@users.noreply.github.com>
2021-06-02 13:22:09 +0100

    Update models.py

--- planitapp/models.py
+++ planitapp/models.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
 
+class planner(models.Model):
+    person = models.OneToOneField(models.User, on_delete = models.CASCADE)
+    data = models.CharField(maxlength = inf)
+    
+    class Meta:
+        verbose_name_plural = 'PLANNERS'
+        
+    def __str__(self):
+        return '{}'.format(self.person')
+'''    
 class planit(models.Model):
     added_date = models.DateTimeField()
     text = models.CharField(max_length=100)
@@ -10,4 +20,4 @@ class planit(models.Model):
 class User(models.Model):
     class planit(models.Model):
         added_date = models.DateTimeField()
-        text = models.CharField(max_length=100)
+        text = models.CharField(max_length=100)'''
