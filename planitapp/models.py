c91a2fcd04de80c8a91b4dc48fe8104f55a3a480
Victor-Agbo <74287630+Victor-Agbo@users.noreply.github.com>
2021-06-19 17:10:26 +0100

    Update models.py

--- planitapp/models.py
+++ planitapp/models.py
@@ -1,18 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class planit(models.Model):
+    added_date = models.DateTimeField()
+    text = models.CharField(max_length=100)
+
 class planner(models.Model):
     inf = 10**10000
-    person = models.OneToOneField(User, on_delete = models.CASCADE)
-    data = models.CharField(max_length = inf)
-    
+    account = models.OneToOneField(User, on_delete=models.CASCADE)
+    data = models.CharField(max_length=inf, blank=True)
+    time = models.CharField(max_length=inf, blank=True)
+
     class Meta:
         verbose_name_plural = 'PLANNERS'
-        
+    
     def __str__(self):
-        return '{}'.format(self.person)
+        return '{}'.format(self.account)
+
 '''    
 class planit(models.Model):
     added_date = models.DateTimeField()
