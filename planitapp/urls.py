f7c889b979494f71528ffeedfbab88403f31b838
Victor-Agbo <74287630+Victor-Agbo@users.noreply.github.com>
2021-07-15 14:56:24 +0100

    Update urls.py

--- planitapp/urls.py
+++ planitapp/urls.py
@@ -6,11 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('user_page/add_planit/', views.add_planit,),
-    path('user_page/delete_planit/<int:planit_id>/', views.delete_planit),
+    path('user_page/delete_planit/', views.delete_planit),
     path('login/', views.login),
     path('log_in/', views.log_in, name='log_in'),
     path('signup/', views.signup),
-    #path('sign_up/', views.sign_up, name='sign_up'),
     path('user_page/', views.user_page, name='user_page')
 ]
     
